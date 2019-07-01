cd4ee7bd5712a41a9bc05b6cf3b2a4ace71efadb
Alexander Endovitsky <endovitsky.a.p@gmail.com>
2019-07-01 17:08:23 +0300

    added demo spawning of buildings

--- Assets/Scripts/Factories/BuildingsFactory.cs
+++ Assets/Scripts/Factories/BuildingsFactory.cs
@@ -1,17 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Managers;
 using UnityEngine;
 
 namespace Factories
 {
     public class BuildingsFactory
     {
+        private List<GameObject> _prefabs = new List<GameObject>();
+
+        private static System.Random _random = new System.Random();
+
         public void Initialize()
         {
-            //
+            _prefabs = GameManager.Instance.BuildingsManager.GetAll().ToList().ConvertAll(x => (GameObject)x);
         }
 
         public GameObject Create()
         {
-            GameObject instance = null;
+            var randomPrefab = _prefabs[_random.Next(0, _prefabs.Count)];
+            var instance = GameObject.Instantiate(randomPrefab);
 
             return instance;
         }
--- Assets/Scripts/Services/CityBuildingService.cs
+++ Assets/Scripts/Services/CityBuildingService.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using Managers;
+using UnityEngine;
 
 namespace Services
 {
@@ -6,7 +8,35 @@ namespace Services
     {
         public void Initialize()
         {
-            //
+            // demo implementation
+            var instances = Create(100, 100, 2);
+        }
+
+        public List<GameObject> Create(int length, int width, int spacing)
+        {
+            var instances = new List<GameObject>();
+
+            for (var x = 0; x < length; x++)
+            {
+                for (var z = 0; z < width; z++)
+                {
+                    var instance = GameManager.Instance.BuildingsFactory.Create();
+                    instance.transform.parent = GameManager.Instance.GameObjectsManager.gameObject.transform;
+
+                    var spawnPositionX = x;
+                    var spawnPositionZ = z;
+                    var positionX = spawnPositionX * instance.gameObject.transform.lossyScale.x *
+                                    spacing;
+                    var positionY = instance.gameObject.transform.lossyScale.y / 2; // height
+                    var positionZ = spawnPositionZ * instance.gameObject.transform.lossyScale.z *
+                                    spacing;
+                    instance.transform.localPosition = new Vector3(positionX, positionY, positionZ);
+
+                    instances.Add(instance);
+                }
+            }
+
+            return instances;
         }
     }
 }
